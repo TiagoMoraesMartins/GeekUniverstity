c8a709c3e821752bda3d4f2e7c4d71da588275b1
Tiago MM <tiago.tmartins@gmail.com>
2024-06-26 23:19:57 -0300

    Ajustando o projeto

--- main.py
+++ main.py
@@ -8,13 +8,68 @@ templates = Jinja2Templates(directory='templates')
 app.mount('/static', StaticFiles(directory='static'), name='static')
 app.mount('/media', StaticFiles(directory='media'), name='media')
 
-@app.get('/')
+@app.get('/', name='index')
 async def index(request: Request):
     context = {
         'request': request
     }
-    
-    return templates.TemplateResponse('home/index.html', context)
+    return templates.TemplateResponse('home/index.html', context=context)
+
+@app.get('/about', name='about')
+async def about(request: Request):
+    context = {
+        "request": request
+    }
+    return templates.TemplateResponse('home/about.html', context=context)
+
+@app.get('/contact', name='contact')
+async def contact(request: Request):
+    context = {
+        "request": request
+    }
+    return templates.TemplateResponse('home/contact.html', context=context)
+
+@app.get('/pricing', name='pricing')
+async def pricing(request: Request):
+    context = {
+        "request": request
+    }
+    return templates.TemplateResponse('home/pricing.html', context=context)
+
+@app.get('/faq', name='faq')
+async def faq(request: Request):
+    context = {
+        "request": request
+    }
+    return templates.TemplateResponse('home/faq.html', context=context)
+
+@app.get('/blog', name='blog')
+async def blog(request: Request):
+    context = {
+        "request": request
+    }
+    return templates.TemplateResponse('home/blog.html', context=context)
+
+@app.get('/blog_post', name='blog_post')
+async def blog_post(request: Request, slug: str=''):
+    context ={
+        "request": request
+    }
+    return templates.TemplateResponse('home/blog_post.html', context=context)
+
+@app.get('/portfolio', name='portfolio')
+async def portfolio(request: Request):
+    context = {
+        "request": request
+    }
+    return templates.TemplateResponse('home/portfolio.html', context=context)
+
+@app.get('/portfolio_item', name='portfolio.html', context=context)
+async def portfolio_item(request:Request):
+    context = {
+        "request": request
+    }
+    return templates.TemplateResponse('home/portfolio_item.html', context=context)
 
 
 
